1a225aca62488631d75dd5bc86d1402a4805edeb
Jinyu Chen <jinyu2010.chen@gmail.com>
2019-09-25 11:33:35 -0500

    remove to sub-folder

---
+++ top_interview_question/1.easy/1.Array/moveZeroes.py
@@ -0,0 +1,28 @@
+"""
+Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Example:
+
+Input: [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Note:
+
+You must do this in-place without making a copy of the array.
+Minimize the total number of operations.
+
+"""
+class Solution:
+    def moveZeroes(self, nums):
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for v in nums:
+            if v == 0:
+                nums.remove(v)
+                nums.append(0)
+
+
+if __name__ == "__main__":
+    nums1 = [0, 0, 1]
+    (Solution().moveZeroes(nums1))
+    print(nums1)
---
+++ top_interview_question/1.easy/1.Array/twoSum.py
@@ -0,0 +1,40 @@
+"""
+Two Sum
+
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+
+"""
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        pos_list = []
+        start = 0
+        while start < len(nums):
+            result_item = target - nums[start]
+            if result_item in nums:
+                result_item_index = nums.index(result_item)
+                if result_item_index != start:
+                    pos_list.extend([start, result_item_index])
+                    nums.remove(nums[start])
+                    nums.remove(result_item)
+                else:
+                    start += 1
+            else:
+                start += 1
+        return pos_list
+
+
+if __name__ == "__main__":
+    nums1 = [3,2,4]
+    target = 6
+    print(Solution().twoSum(nums1, target))
