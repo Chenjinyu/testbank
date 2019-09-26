f80c4c7e67b381e5ee8722c6d5f153b0128b5037
Jinyu Chen <jinyu2010.chen@gmail.com>
2019-09-25 21:17:38 -0500

    string test

---
+++ top_interview_question/1.easy/2.Strings/387.firstUniqueCharacterInaString.py
@@ -0,0 +1,55 @@
+"""
+Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+
+Examples:
+
+s = "leetcode"
+return 0.
+
+s = "loveleetcode",
+return 2.
+Note: You may assume the string contain only lowercase letters.
+
+memory test:  python -m memory_profiler 387.firstUniqueCharacterInaString.py
+======
+Filename: 387.firstUniqueCharacterInaString.py
+
+Line #    Mem usage    Increment   Line Contents
+================================================
+    18   11.461 MiB   11.461 MiB       @profile
+    19                                 def firstUniqChar(self, s: str) -> int:
+    20   11.461 MiB    0.000 MiB           s_counter = Counter(s)
+    21   11.469 MiB    0.008 MiB           print(s_counter)
+    22   11.469 MiB    0.000 MiB           if 1 not in s_counter.values():
+    23                                         return -1
+    24
+    25   11.469 MiB    0.000 MiB           matched_index = []
+    26   11.469 MiB    0.000 MiB           for k, v in s_counter.items():
+    27   11.469 MiB    0.000 MiB               if v == 1:
+    28   11.469 MiB    0.000 MiB                   matched_index.append(s.find(k))
+    29
+    30   11.469 MiB    0.000 MiB           return min(matched_index)
+
+"""
+
+from collections import Counter
+
+class Solution:
+    @profile
+    def firstUniqChar(self, s: str) -> int:
+        s_counter = Counter(s)
+        print(s_counter)
+        if 1 not in s_counter.values():
+            return -1
+
+        matched_index = []
+        for k, v in s_counter.items():
+            if v == 1:
+                matched_index.append(s.find(k))
+
+        return min(matched_index)
+
+
+if __name__ == "__main__":
+    s = "leetcode"
+    print(Solution().firstUniqChar(s))
---
+++ top_interview_question/1.easy/2.Strings/7.reverseInteger.py
@@ -0,0 +1,32 @@
+"""
+Given a 32-bit signed integer, reverse digits of an integer.
+
+Example 1:
+
+Input: 123
+Output: 321
+Example 2:
+
+Input: -123
+Output: -321
+Example 3:
+
+Input: 120
+Output: 21
+Note:
+Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range:
+[−2**31,  2**31 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+
+"""
+class Solution:
+    def reverse(self, x: int) -> int:
+        reversed = int("".join(list(str(abs(x)))[::-1]))
+        if x < 0:
+            return max(-reversed, -2**31) if max(-reversed, -2**31) > -2**31 else 0
+        return min(reversed, 2**31 - 1) if min(reversed, 2**31 - 1) < 2**31 - 1 else 0
+
+if __name__ == "__main__":
+    x = -1534236469
+    print(Solution().reverse(x))
+
+
