4e323c0e24e5052cffb423e5c1846fd3818514e4
Jinyu Chen <jinyu2010.chen@gmail.com>
2019-09-30 21:37:19 -0500

    greedy with prefix sums

--- README.md
+++ README.md
@@ -4,3 +4,21 @@ Must to understand:
 #### Prefix Sums
 1. [SubArrary Sum Equals K](./top_interview_question/3.hard/560.subarraySumEqualsK.py) 
 2. [SubArray Sum Divisible By K](./top_interview_question/2.medium/974.subarraySumsDivisibleByK.py)
+
+#### Greedy
+**Intuition**
+The problem to find maximum(or minimum) element (or sum) with a single array as the input is a good candidate to 
+be solved by the greedy approach in linear time.
+```
+Pick the locally optimal move at each step, and that will lead to the globally optimal solution.
+```
+
+the algorithm is general and straightforward: **iterate over the array and update each step and standard set 
+for such problems**:
+* Current element
+* Current local maximum sum (at this given point)
+* Global maximum sum seen so far.
+
+![greedy](./img/greedy.png)
+
+1. [Maximum Subarray](./top_interview_question/1.easy/1.Array/53.maximumSubarray.py)
---
+++ top_interview_question/1.easy/1.Array/53.maximumSubarray.py
@@ -0,0 +1,43 @@
+"""
+53. Maximum Subarray
+
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+Example:
+
+Input: [-2,1,-3,4,-1,2,1,-5,4],
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+Follow up:
+
+If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+"""
+
+from typing import List
+from itertools import accumulate
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        prefix_sums_list = list(accumulate(nums))
+        print(prefix_sums_list)
+        return max(prefix_sums_list) - min(prefix_sums_list)
+
+    def maxSubArrayImprove(self, nums: List[int]) -> int:
+        length = len(nums)
+        # consider when length of list equals 1.
+        cur_sum = max_sum = nums[0]
+        # starts from pos = 1, because pos 0 already taken.
+        for i in range(1, length):
+            #
+            cur_sum = max(nums[i], cur_sum + nums[i])
+            max_sum = max(cur_sum, max_sum)
+        return max_sum
+
+
+if __name__ == "__main__":
+    # nums = [-2,1,-3,4,-1,2,1,-5,4]
+    # nums = [-2,1]
+    nums = [-2, -1]
+    print(Solution().maxSubArrayImprove(nums))
