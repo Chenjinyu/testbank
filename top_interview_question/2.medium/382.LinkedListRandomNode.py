7d4d812f1c85430d2e91a3c464955c80ad7be94f
Jinyu Chen <jinyu2010.chen@gmail.com>
2019-10-10 15:27:46 -0500

    must understand the logic.

--- top_interview_question/2.medium/382.LinkedListRandomNode.py
+++ top_interview_question/2.medium/382.LinkedListRandomNode.py
@@ -16,6 +16,20 @@ Solution solution = new Solution(head);
 
 // getRandom() should return either 1, 2, or 3 randomly. Each element should have equal probability of returning.
 solution.getRandom();
+
+随机采样算法 Reservoir Sampling, 论点
+
+假设现在要从 n 个数里面随机选择一个数，那么通过 Reservoir sampling 选择的流程如下:
+
+1. 记最终选择的数为 result
+2. 遍历数组，对于数组第 i 个数，以 1/i 的概率将其赋给result（i从1开始，所以第一个数肯定会赋给result）
+3. 遍历完数组后得到的 result 即为产生的随机数
+
+假设现在有数组 [1, 2, 3], 随机产生一个数，那么按照上面的流程有
+
+1. 遍历第一个数时，result = 1
+2. 遍历第二个数时，result = 2 的概率为 1/2, 即 result = 1 的概率也是 1/2
+3. 遍历第三个数时，result = 3 的概率为 1/3, result = 1 的概率为 (1 - 1/3) * 1/2 = 1/3, 同理 result = 2 的概率也是 1/3
 """
 import random
 
@@ -35,17 +49,17 @@ class Solution:
         @param head The linked list's head.
         Note that the head is guaranteed to be not null, so it contains at least one node.
         """
-        dummy.next = head
+        self.dummy.next = head
 
     def getRandom(self) -> int:
         """
         Returns a random node's value.
         """
-        curr_node = dummy.next
+        curr_node = self.dummy.next
         count = result = 0
         while curr_node:
             count += 1
-            if random.random() < (1.0/count):
+            if random.random() < (1.0 / count):
                 result = curr_node.val
                 
             curr_node = curr_node.next
