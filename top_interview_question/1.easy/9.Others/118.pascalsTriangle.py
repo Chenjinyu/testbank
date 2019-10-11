9fb8a6fe1b9facf45c9760ba7a93198360f37666
Jinyu Chen <jinyu2010.chen@gmail.com>
2019-10-11 13:09:57 -0500

    Update 118.pascalsTriangle.py

--- top_interview_question/1.easy/9.Others/118.pascalsTriangle.py
+++ top_interview_question/1.easy/9.Others/118.pascalsTriangle.py
@@ -14,10 +14,49 @@ Output:
   [1,3,3,1],
  [1,4,6,4,1]
 ]
+
+Note: 
+generateMostEfficient uses list.append(), it's more efficient than create the two dimensional list first.
+because, compare with generateFirstAccpeted(what i did), it create items with the same length of item.
+think about if the length is too much long, it not only comsume a lot of space, aslo the time to search the memory
+and return a pointer to you.
+
+1). list[index] = [1,2,3] vs 2). list.append([1,2,3])
+1). first of all, python will search the address of index points to, and delete the connection between of them, 
+and assgin new space for you.
+2). find a space and return/assgin the ponter back, it's much faster than 1).
+
 """
 
 class Solution:
-    def generate(self, numRows: int) -> List[List[int]]:
+    def generateMostEfficient(self, numRows: int) :
+        if numRows == 0:
+            return None
+        for i in range(numRows):
+            if i < 1:
+                result = [[1]]
+            else:
+                result.append([0] * (i + 1))
+                result[i][0], result[i][-1] = 1, 1
+                for j in range(1, i):
+                    result[i][j] = result[i - 1][j - 1] + result[i - 1][j]
+        return result
+    
+    
+    def generateMyIdeaFirstInMind(self, numRows: int):
+        # this is what I want to implement at the beginning of thinking.
+        result = []
+        for i in range(numRows):
+            if not result:
+                result.append([1])
+            else:
+                result.append(
+                    [1] + [result[i - 1][j - 1] + result[i - 1][j] for j in range(1, len(result[i - 1]))] + [1])
+        return result
+    
+    
+    def generateFirstAccpeted(self, numRows: int):
+        # test = 5 * [0] ==> [0, 0, 0, 0, 0] vs test = 5 * [[0]] ==> [[0], [0], [0], [0], [0]]
         if numRows == 0:
             return []
         elif numRows == 1:
@@ -41,6 +80,25 @@ class Solution:
 
         return triangle_list
 
+
+    def generateLogicImprove(self, numRows):
+        triangle = []
+
+        for row_num in range(numRows):
+            # set item are None, and the first and last row elements are ALWAYS 1.
+            row = [None for _ in range(row_num + 1)]
+            row[0], row[-1] = 1, 1
+
+            # Each triangle element is equal to the sum of the elements
+            # above-and-to-the-left and above-and-to-the-right.
+            for j in range(1, len(row) - 1):
+                row[j] = triangle[row_num - 1][j - 1] + triangle[row_num - 1][j]
+
+            triangle.append(row)
+
+        return triangle
+
+    
 if __file__ == "__main__":
     n = 30
     print(Solution().generate(n))
