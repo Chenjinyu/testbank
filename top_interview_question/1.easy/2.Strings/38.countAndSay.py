9fdd49c7ccd0f8431c7ea1be39497c93461a4b78
Jinyu Chen <jinyu2010.chen@gmail.com>
2019-10-11 21:11:12 -0500

    understand the sequence which relys on last sequence.

--- top_interview_question/1.easy/2.Strings/38.countAndSay.py
+++ top_interview_question/1.easy/2.Strings/38.countAndSay.py
@@ -1,6 +1,5 @@
 """
 The count-and-say sequence is the sequence of integers with the first five terms as following:
-
 1.     1
 2.     11
 3.     21
@@ -14,8 +13,6 @@ Given an integer n where 1 ≤ n ≤ 30, generate the nth term of the count-and-
 
 Note: Each term of the sequence of integers will be represented as a string.
 
-
-
 Example 1:
 
 Input: 1
@@ -24,10 +21,31 @@ Example 2:
 
 Input: 4
 Output: "1211"
+
 """
 
-from collections import Counter
+
 class Solution:
     def countAndSay(self, n: int) -> str:
-        n_counter = Counter([v for v in str(n)])
-        return str(sum(n_counter.values()))
+        seq = "1"
+        # the range from n - 1
+        # because every Sequence have to get from the last Sequence.
+        for i in range(n - 1):
+            seq = self.seqNext(seq)
+        return seq
+
+    def seqNext(self, seq):
+        i, next_seq = 0, ""
+        while i < len(seq):
+            count = 1
+            while i < len(seq) - 1 and seq[i] == seq[i+1]:
+                count += 1
+                i += 1
+            next_seq += str(count) + seq[i]
+            i += 1
+        return next_seq
+
+
+if __name__ == "__main__":
+    n = 6
+    print(Solution().countAndSay(n))
