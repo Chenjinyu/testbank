d7f538269937267b9ae70375629032982918f241
Jinyu Chen <jinyu2010.chen@gmail.com>
2019-12-31 16:29:11 -0600

    minhead of heapq

--- 0253.meetingRoomsII_m.py
+++ 0253.meetingRoomsII_m.py
@@ -35,8 +35,8 @@ class Solution:
         heapq.heappush(free_room, intervals[0][1])
 
         for interval in intervals[1:]:
-            # compare with the first item in free_room which is the end time.
-            # 1. 30, 2. 10 -> [10,30]. 3. 10 < 15, pop. so the len is 2
+            # compare with the min sorted list by first item in free_room which is the end time.
+            # 1. push 17. 2. push 5, [5,17], 3. 5 > 4, push 8, [5,8,17], 4. 5 > 4: push 5. so [5,5,8,17]
             if free_room[0] <= interval[0]:
                 heapq.heappop(free_room)
 
@@ -47,6 +47,7 @@ class Solution:
 
 if __name__ == "__main__":
     testcases = [
+        {'s': [[4, 17], [4, 5], [4, 8], [4, 5]], 'e': 4},
         {'s': [[4, 9], [4, 17], [8, 10], [9, 18]], 'e': 3},
         {'s': [[0, 30], [5, 10], [15, 20]], 'e': 2},
         {'s': [[9, 10], [4, 9], [4, 17]], 'e': 2},
