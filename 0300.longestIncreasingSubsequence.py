b69a4986d5cc100eab02713d0ff15e8e7b91575f
Jinyu Chen <jinyu2010.chen@gmail.com>
2020-01-06 15:46:14 -0600

    dp good example

--- 0300.longestIncreasingSubsequence.py
+++ 0300.longestIncreasingSubsequence.py
@@ -14,22 +14,29 @@ There may be more than one LIS combination, it is only necessary for you to retu
 Your algorithm should run in O(n2) complexity.
 Follow up: Could you improve it to O(n log n) time complexity?
 """
-from typing import List
-
 class Solution:
-    def lengthOfLIS(self, nums: List[int]) -> int:
-        left, right = 0, 1
-        count = 0
-        while right < len(nums):
-            if nums[left] < nums[right]:
-                right += 1
-                count += 1
-            else:
-                left += 1
-                right += 1
-        return count
-
+    def longestIncreaseSubsequenceDP(nums):
+    """
+    Time Complexity: O(N^2)
+    Space Complexity: O(N)
+    """
+    if not nums: return 0
+    # set dp list default as 1. coz, only has it self will return 1.
+    dp = [1] * len(nums)
+    # 这道题无法避免的需要两次loop。从nums的第一个开始，查看它之前是否有小于它的数
+    # 如果有，就求出 dp[i] = max(dp[i], dp[j] + 1)
+    # 比如：当i 为3时，dp[i] = max(dp[i], dp[1]的值 + 1) 和 dp[i] = max(dp[i], dp[2]的值 + 1), 取其最大值。
+    for i in range(len(nums)):
+        for j in range(i):
+            if nums[i] > nums[j]:
+                dp[i] = max(dp[i], dp[j] + 1)
+    return max(dp)
 
 if __name__ == "__main__":
-    nums = [10,9,2,5,3,7,101,18]
-    print(Solution().lengthOfLIS(nums))
+    testcases = [
+        {'nums': [1, 5, 3, 4, 6, 9, 7, 8], 'e': 6},
+        {'nums': [6, 5, 3, 4, 6, 9, 7, 8], 'e': 5},
+    ]
+    for case in testcases:
+        actual_result = longestIncreaseSubsequenceDP(case['nums'])
+        print("Input: {}\nExpected: {}\nAcutal: {}\nis_Passed: {}".format(case['nums'],case['e'], actual_result, actual_result== case['e']))
