dbb138a7832fd2fba8206af04b17e079d2978ef1
Jinyu Chen <jinyu2010.chen@gmail.com>
2020-01-13 22:02:14 -0600

    Update 0460.LFUCache_h.py

--- 0460.LFUCache_h.py
+++ 0460.LFUCache_h.py
@@ -79,3 +79,20 @@ class LFUCache:
 # obj = LFUCache(capacity)
 # param_1 = obj.get(key)
 # obj.put(key,value)
+
+if __name__ == "__main__":
+    """
+    ["LFUCache","put","put","get","put","get","get","put","get","get","get"]
+    [[2],[1,1],[2,2],[1],[3,3],[2],[3],[4,4],[1],[3],[4]]
+    """
+    l = LFUCache(2)
+    l.put(1, 1)
+    l.put(2, 2)
+    l.get(1)
+    l.put(3, 3)
+    l.get(2)
+    l.get(3)
+    l.put(4, 4)
+    l.get(1)
+    l.get(3)
+    l.get(4)
