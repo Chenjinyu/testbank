4821f04b1e3e1d5024aad311c184ff97d35f5c76
Jinyu Chen <jinyu2010.chen@gmail.com>
2020-01-28 11:50:38 -0600

    Update 20.Particle Velocity.py

---
+++ Microsoft_OA/20.Particle Velocity.py
@@ -0,0 +1,13 @@
+"""
+You are a programmer in a scientific team doing research into particles.
+As an experiment, you have measured the position of a single particle in N equally distributed moments of time.
+The measurement made in moment K is recorded in array A as A[K]
+
+Now, your job is to count all the periods of time when the movement of the particle was stable.
+those are the periods during which the particle doesn't change its velocity. 
+i.e. the different betwwen any two consecutive position measurements remains the same.
+Note that you need at least three measurements to be sure that the particle did not change its velocity.
+
+Examples:
+
+"""
