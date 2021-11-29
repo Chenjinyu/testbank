fe15af92c7cb8296d0470388c01db4e23fe465c9
Jinyu Chen <jinyu2010.chen@gmail.com>
2021-11-28 23:08:22 -0600

    dfs for the matrix

--- 1167.minimumCostToConnectSticks_m.py
+++ 1167.minimumCostToConnectSticks_m.py
@@ -11,10 +11,19 @@ Return the minimum cost of connecting all the given sticks into one stick in thi
 Example 1:
 Input: sticks = [2,4,3]
 Output: 14
+Explanation: You start with sticks = [2,4,3].
+1. Combine sticks 2 and 3 for a cost of 2 + 3 = 5. Now you have sticks = [5,4].
+2. Combine sticks 5 and 4 for a cost of 5 + 4 = 9. Now you have sticks = [9].
+There is only one stick left, so you are done. The total cost is 5 + 9 = 14.
 
 Example 2:
 Input: sticks = [1,8,3,5]
 Output: 30
+Explanation: You start with sticks = [1,8,3,5].
+1. Combine sticks 1 and 3 for a cost of 1 + 3 = 4. Now you have sticks = [4,8,5].
+2. Combine sticks 4 and 5 for a cost of 4 + 5 = 9. Now you have sticks = [9,8].
+3. Combine sticks 9 and 8 for a cost of 9 + 8 = 17. Now you have sticks = [17].
+There is only one stick left, so you are done. The total cost is 4 + 9 + 17 = 30.
 
 
 Constraints:
--- DFS_Matrix.py
+++ DFS_Matrix.py
@@ -25,11 +25,38 @@ def dfs_deque(test):
         ans += " " + str(test[r][c])
         visited[r][c] = True
         # stack.appendleft((r, c - 1))  # left
+        # go right is the last step
         stack.appendleft((r, c + 1))  # right
+        # second step in the deque queue, when the first down finish, it will go up, but all of them are True.
+        # so, until pop the right grid.
         stack.appendleft((r - 1, c))  # up
         stack.appendleft((r + 1, c))  # down
 
     return ans
+
+
+def dfs_deque_right_append(test):
+    # depth-first-search in 2D matrix
+    row, col = len(test), len(test[0])
+    ans = ""
+    visited = np.full((row, col), False, dtype=bool)
+    # visited = np.full((2, 2), 'c', dtype=str)  # full fills matrix with different types
+    stack = deque([(0, 0)])
+    while stack:
+        r, c = stack.pop()
+
+        if r < 0 or r >= row or c < 0 or c >= col or visited[r][c]:
+            continue
+
+        ans += " " + str(test[r][c])
+        visited[r][c] = True
+        stack.append((r, c + 1))  # right
+        stack.append((r - 1, c))  # up
+        stack.append((r + 1, c))  # down
+
+
+    return ans
+
 
 
 def dfs(test):
@@ -55,5 +82,5 @@ def dfs(test):
     return ans
 
 
-print(dfs_deque(test))
+print(dfs_deque_right_append(test))
 print(" 1 5 9 13 14 10 6 2 3 7 11 15 16 12 8 4")
