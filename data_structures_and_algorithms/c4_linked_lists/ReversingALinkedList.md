5b4a1a47c04f8dc84563a675205533b6d7d2e021
Jinyu Chen <jinyu2010.chen@gmail.com>
2023-03-03 00:46:40 -0600

    start working on the reversing a linkedlist

---
+++ data_structures_and_algorithms/c4_linked_lists/ReversingALinkedList.md
@@ -0,0 +1,4 @@
+## Reversing a linked list
+
+While reversing a linked list is a common interview problem in itself, it is also a technique that can be a step in solving different problems. Elegantly performing the reversal requires a good understanding of how to move pointers around, which we will aim to achieve in this article.
+
--- tests_2023/test_c4_linked_lists.py
+++ tests_2023/test_c4_linked_lists.py
@@ -1,6 +1,8 @@
 import pytest
 
 from data_structures_and_algorithms.c4_linked_lists.de0141_LinkedListCycle import Solution as Sol0141
+from data_structures_and_algorithms.c4_linked_lists.e0876_MiddleOfTheLinkedList import Solution as Sol0876
+from data_structures_and_algorithms.c4_linked_lists.e0083_RemoveDuplicatesFromSortedList import Solution as Sol0083
 
 @pytest.mark.parametrize("test_input1, excepted_output", [
     ([3,2,0,-4], True),
@@ -9,3 +11,8 @@ from data_structures_and_algorithms.c4_linked_lists.de0141_LinkedListCycle impor
 ])
 def test_hasCycle(test_input1, excepted_output):
     assert Sol0141().hasCycle(test_input1) == excepted_output
+    
+    
+    
+    
+    
