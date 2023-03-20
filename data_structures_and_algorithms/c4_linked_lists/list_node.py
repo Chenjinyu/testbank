d09e4a31e829e56598dc39c4cd82b4227c6bea16
Jinyu Chen <jinyu2010.chen@gmail.com>
2023-03-20 09:16:54 -0500

    class object overwirtten the __iter__ and the __repr__

--- data_structures_and_algorithms/c4_linked_lists/list_node.py
+++ data_structures_and_algorithms/c4_linked_lists/list_node.py
@@ -15,7 +15,7 @@ class ListNode:
 
 class LinkedList:
     # Union[int, str, None] == Optional[Union[int, str]]
-    def __init__(self) -> None:
+    def __init__(self):
         self.head = None
         print('__init__ is called')
     
@@ -23,14 +23,25 @@ class LinkedList:
     # def __new__(cls, node_list: List[Optional[Union[int, str]]]) -> None:
     #     print('__new__ is called')
         
-    
     def __iter__(self):
+        # self returns the LinkedListNodes itself, but as defalut, class object is not iterable, until overwrite the __iter__()
         node = self.head
         while node:
             yield node
             node = node.next
             
-    def add_to_tail(self, node: ListNode):
+    def is_empty(self) -> bool:
+        return self.head == None
+            
+    def add_to_tail(self, node: Union[ListNode, int]):
+        """
+        Add an ListNode to the end(push). 
+        1. if only has the head attr, then the new node to the end need to loop to the end, and add the new node to the next.
+        2. if has head and tail, the LinkedList needs to maintenance the head and tail everytime when the node changes.
+        """
+        if not isinstance(node, ListNode):
+            node = ListNode(node)
+            
         if self.head == None:
             self.head = node
             return
@@ -47,8 +58,18 @@ class LinkedList:
         self.head = self.head.next # move to the next node, the first node will be isolated.
         return temp
     
+    def to_list(self) -> List[int]:
+        result = []
+        
+        # self returns the LinkedListNodes itself, but as defalut, class object is not iterable, until overwrite the __iter__()
+        for cur_node in self:
+            result.append(cur_node.val)
+        return result
     
     def __repr__(self) -> str:
+        """
+        __repr__ method returns a string representation of the class object. 
+        """
         if not self:
             return "[]"
         nodes = []
@@ -58,6 +79,68 @@ class LinkedList:
         return("[" + ", ".join(nodes) + "]")
     
     
+class LinkedListNodes:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            node = node.next
+                    
+    def is_empty(self) -> bool:
+        return self.head == None
+    
+    def push(self, new_node:Union[ListNode, int]):
+        if not isinstance(new_node, ListNode):
+            new_node = ListNode(new_node)
+        
+        if self.is_empty():
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+            
+    def pop(self) -> int:
+        """Pop value from the LinkedListNode tail """
+        if self.is_empty():
+            return None
+        
+        val = self.tail.value
+        
+        # everytime operates the node, the linkedListNodes needs to maintenance. 
+        if self.tail == self.head:
+            self.tail = None
+            self.head = None
+        else:
+            cur_node = self.head
+            
+            while cur_node != self.tail:
+                cur_node = cur_node.next
+            
+            self.tail = cur_node
+            self.tail.next = None   # disconect the last one node.
+            
+        return val            
+    
+    def to_list(self) -> List[int]:
+        result = []
+        
+        # self returns the LinkedListNodes itself, but as defalut, class object is not iterable, until overwrite the __iter__()
+        for cur_node in self:
+            result.append(cur_node.val)
+        
+        return result
+    
+    
+    def __repr__(self) -> str:
+        values = self.to_list()
+        return 'LinkedList([' + ', '.join(str(value) for value in values) + '])'
+
+
 def build_linked_list(node_list) -> ListNode:
     if not len(node_list):
         return None
@@ -70,7 +153,17 @@ def build_linked_list(node_list) -> ListNode:
     #     return self.node_list
             
         
+def build_linked_list_node(node_list) -> ListNode:
+    if not len(node_list):
+        return None
+    linked_list_nodes = LinkedListNodes()
+    for node in node_list:
+        linked_list_nodes.push(ListNode(node))
+    return linked_list_nodes
+    
 
+test_list = build_linked_list_node([1,2,3])
+print(test_list.to_list())    
 # print('________bases________')            
 # print(dir(LinkedList.__bases__))
 # print('________getatrribute________')
